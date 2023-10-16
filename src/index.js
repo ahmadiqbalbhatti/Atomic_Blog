fe4bb3545ee0965ec6e420b6b145163a77e5d62c
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-10-16 21:02:26 +0500

    useMemo and useCallback Hooks

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
+import MemoApp from "./MemoApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <MemoApp/>
   </React.StrictMode>
 );
